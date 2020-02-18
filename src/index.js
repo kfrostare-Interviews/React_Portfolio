7ae3698a4b589bd22eb13df08635cc0c5e82e690
karolina <karolina.frostare@gmail.com>
2020-02-18 05:40:58 +0100

    Adds navigation to new pages adds link and navlink components

--- src/index.js
+++ src/index.js
@@ -3,17 +3,22 @@ import ReactDOM from "react-dom";
 import Hello from "./Hello";
 import Header from "./Header";
 import Footer from "./Footer";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import Projects from "./Projects";
 import About from "./About";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
+    <>
       <Header />
-      <Hello />
+      <Switch>
+        <Route exact path="/" component={Hello}></Route>
+        <Route exact path="/about" component={About}></Route>
+        <Route exact path="/projects" component={Projects}></Route>
+      </Switch>
       <Footer />
-    </div>
+    </>
   );
 };
 

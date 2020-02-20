d3b92736d4c49e166fae5d3e9a8d1228eb26adc5
karolina <karolina.frostare@gmail.com>
2020-02-20 09:00:10 +0100

    update header in css and content in About

--- src/About.jsx
+++ src/About.jsx
@@ -6,21 +6,21 @@ const About = () => {
       <div class="ui main container">
         <div class="column">
           <h1 className="introHeader">Say Karro</h1>
+          
           <p>
-            I am 34 years old living in Stockholm with my husband with whom I travel a lot as this is our shared passion. 
-            During our travels I love taking photos of people, places, animals or just amazing scenery. My name is Karolina
+            I am a 30-somethin' year old living in Stockholm with my husband, with whom I travel a lot as this is our shared passion. 
+            During our travels I love taking photos of people, places, animals or just beautiful scenery. My name is Karolina
             but please say Karro.</p>
-            
-            <h2>Passion number three</h2> 
-            
+          
+          <h2>Passion number three</h2> 
+          
           <p>
             would be to create for the underdog, the small businesses who don't themselves have the time to dive into 
             details like the look and feel of a business card or a website. I enjoy finding people with talent and helping them out with their
-            marketing in exchange for their story. The most amazing thing is when you ask a small-business owner to tell you their journey. 
-            The story you'll hear is one of sacrifice in blood, sweat and tears to make their dream come true. And most importantly they will
-            speak of the love they have for the profession. After that, helping them create an app or design a business card in line with their 
-            brand is in a way to be part of their journey and dream. So rewarding and amazing!
+            marketing in exchange for me to be a part of their story. Helping them create an app or design a business card in line with their 
+            brand is in a way to be part of their journey and life long dream. That, to me, is so rewarding!
           </p>
+
         </div>
       </div>
     </div>
--- src/Reviews.jsx
+++ src/Reviews.jsx
@@ -7,9 +7,8 @@ const Reviews = () => {
         <div class="column">
           <h1 className="introHeader">Reviews</h1>
           <p>
-            Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident
-            corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum
-            molestias?
+            They say true friends are the ones who talk shit about you to your face and complements you behind your back.
+            Well I forced my former colleagues to dish about some things they say when I'm not around and here are some of their stories.  
           </p>
         </div>
       </div>

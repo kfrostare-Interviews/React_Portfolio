d8c87850e90d591328e03d5e510d43a810fdeb86
karolina <karolina.frostare@gmail.com>
2020-02-23 10:44:32 +0100

    updates cypress tests for cv cards, all green

--- cypress/integration/userCanNavigateCvList.feature.js
+++ cypress/integration/userCanNavigateCvList.feature.js
@@ -1,4 +1,4 @@
-describe('User can see list of previous employments', () => {
+describe('User can see list of previous education and employments', () => {
     beforeEach(() => {
       cy.visit('http://localhost:3000');
       cy.get('#cv-tab').click()
@@ -7,24 +7,24 @@ describe('User can see list of previous employments', () => {
     it('displays last place of employment', () => {
       cy.get('#cvCards1').within(() => {
         cy.get(('.image')).should('exist');
-        cy.get('.ui.header').should('contain', '...');
-        cy.get('.description').should('contain', '...');
+        cy.get('.ui.header').should('contain', 'Craft Academy');
+        cy.get('.description').should('contain', 'BootCamp focused on learning');
       })
     });
   
     it('displays second last place of employment', () => {
       cy.get('#cvCards2').within(() => {
         cy.get(('.image')).should('exist');
-        cy.get('.ui.header').should('contain', '...');
-        cy.get('.description').should('contain', '...');
+        cy.get('.ui.header').should('contain', 'PlantVision');
+        cy.get('.description').should('contain', 'Marketing coordinator at tech');
       })
     });
   
     it('displays third last place of employment', () => {
       cy.get('#cvCards3').within(() => {
         cy.get(('.image')).should('exist');
-        cy.get('.ui.header').should('contain', '...');
-        cy.get('.description').should('...');
+        cy.get('.ui.header').should('contain', 'Tritech');
+        cy.get('.description').should('contain', 'Marketing coordinator at high-tech dev');
       })
     });  
   });
